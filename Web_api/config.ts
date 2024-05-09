cf3f98729622d84bb03be6cbf8bd796d9874ceaf
SiriusTimeStop <jackychoikalok@gmail.com>
2024-05-09 23:08:44 +0800

    first upload

---
+++ Web_api/config.ts
@@ -0,0 +1,28 @@
+// export const config = {
+//   host: "localhost",
+//   port: 5432,
+//   user: "postgres",
+//   password: '27547818',
+//   database: "Coursework",
+//   connection_limit: 100
+// }
+
+
+export const config = {
+  host: "kesavan.db.elephantsql.com",
+ port: 5432,
+ user: "kmtctvtb",
+ password: "5reK7D7m26GvnZSc_07rmTs0vypF3hHY",
+ database: "kmtctvtb",
+ connection_limit: 100
+}
+
+/*
+export const configMongo = {
+  user: 'testuser',
+  pwd:  'mongoDB1688',
+  host: '304cem.jau1i.mongodb.net',
+  dbname: 'blog'
+}
+
+*/
